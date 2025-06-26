52342e79b3617f1ac86a75ab5cf5c1994379389b
Felo Badino <badfelo@gmail.com>
2025-06-26 16:00:01 -0300

    Changing the language of some labels

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 resultado = None
 
 # Mostrar una gran imagen en la parte superior.
-st.image('./Media/portada.png', use_container_width=True)
+st.image('./Media/perfect_roommate.png', use_container_width=True)
 
 st.markdown(f'<div style="text-align: right;">By Felo</div>', unsafe_allow_html=True)
 # Insertar un espacio vertical de 60px
@@ -17,19 +17,22 @@ st.markdown(f'<div style="margin-top: 60px;"></div>', unsafe_allow_html=True)
 
 # Configurar el sidebar con inputs y un botón.
 with st.sidebar:
-    st.header("¿Quién está viviendo ya en el piso?")
-    inquilino1 = st.text_input("Id Inquilino 1")
-    inquilino2 = st.text_input("Id Inquilino 2")
-    inquilino3 = st.text_input("Id Inquilino 3")
+    st.header("Who already lives in the apartment?")
+    inquilino1 = st.text_input("Id Tenant 1")
+    inquilino2 = st.text_input("Id Tenant 2")
+    inquilino3 = st.text_input("Id Tenant 3")
     
-    num_compañeros = st.text_input("¿Cuántos nuevos compañeros quieres buscar?")
+    num_compañeros = st.text_input("How many roommates do you want to search for?")
+    col1, col2, col3 = st.sidebar.columns([1, 2, 1]) # Proporciones: izquierda, centro, derecha
+    with col2: # El botón se colocará en la columna central
+        valor_boton = st.button("Search")
     
-    if st.button('BUSCAR NUEVOS COMPAÑEROS'):
+    if valor_boton: 
         # Verifica que el número de compañeros sea un valor válido
         try:
             topn = int(num_compañeros)
         except ValueError:
-            st.error("Por favor, ingresa un número válido para el número de compañeros.")
+            st.error("Please, enter a valid number as the amount of roommates.")
             topn = None
         
         # Obtener los identificadores de inquilinos utilizando la función
@@ -47,12 +50,12 @@ elif resultado is not None:
     cols = st.columns((1, 2))  # Divide el layout en 2 columnas
     
     with cols[0]:  # Esto hace que el gráfico y su título aparezcan en la primera columna
-        st.write("Nivel de compatibilidad de cada nuevo compañero:")
+        st.write("Compatibility level of each new roommate:")
         fig_grafico = generar_grafico_compatibilidad(resultado[1])
         st.pyplot(fig_grafico)
     
     with cols[1]:  # Esto hace que la tabla y su título aparezcan en la segunda columna
-        st.write("Comparativa entre compañeros:")
+        st.write("Comparison between all roommates:")
         fig_tabla = generar_tabla_compatibilidad(resultado)
         st.plotly_chart(fig_tabla, use_container_width=True)
 
